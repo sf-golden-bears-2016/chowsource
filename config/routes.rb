241e09c4fafa65fc98d6c2066eebc8c6c768616d
Alexander Pellas <email": null>
2016-08-04 15:16:30 -0700

    Update route for recipes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,7 @@ Rails.application.routes.draw do
   get '/sessions/logout', to: 'sessions#delete'
   root "sessions#login"
   resources :recipes
-
-  # get 'recipes/show'
-
-  # get 'recipes/edit'
-
-  # get 'recipes/index'
-
-  # get 'recipes/new'
-
-  # get 'recipes/create'
-
-  # get 'recipes/destroy'
-
-  # get 'recipes/update'
-
-  get 'users/show'
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
