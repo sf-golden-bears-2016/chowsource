be82e6ae1420ec0476779b7ab630a76210b5e498
Alexander Pellas <email": null>
2016-08-05 16:16:13 -0700

    Add ability to love/unlove recipes.

--- db/migrate/20160805190546_create_loves.rb
+++ db/migrate/20160805190546_create_loves.rb
@@ -4,6 +4,8 @@ class CreateLoves < ActiveRecord::Migration[5.0]
       t.integer :user_id
       t.integer :recipe_id
       t.timestamps
+
+      add_index(:loves, [:user_id, :recipe_id], unique: true)
     end
   end
 end
