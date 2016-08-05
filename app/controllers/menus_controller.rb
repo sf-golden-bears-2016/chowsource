9c3a991ad5083d34c0aa87660b6b6919a4bc9721
Georgia Rust & Will Friebel <pair+grust101.wfriebel@devbootcamp.com>
2016-08-05 13:26:36 -0700

    Create ability to add recipes to menu and see menu.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,12 +2,13 @@ class MenusController < ApplicationController
 	def create
     user = current_user
     recipe = Recipe.find(params[:id])
-    user.menu_recipes << recipe
-		redirect_to "/users/menu"
-	end
+    user.featured_recipes << recipe
+    @menu_recipes = user.featured_recipes
+    render template: "/menus/show"
+  end
 
-	def show
-	end
+  def show
+  end
 
 end
 
